871dc2d46af5dfe5b3301727128b096e81c8af57
Your Name <garettm123@gmail.com>
2021-05-13 14:24:46 -0500

    added neat python compilation script

---
+++ publishSite.py
@@ -0,0 +1,32 @@
+# publishSite.py
+#
+# I want to not include google analytics for local test site so I have 2 different config files
+# config.toml, the default, has no analytics code
+# publishConfig.toml had analytics, and the netlify publish command is set to use it
+#
+#It also automatically runs the commands to push the site, except I still enter my credentials by hand for obvious reasons
+
+import os
+import sys
+import subprocess as sp
+
+#Make publishConfig.toml
+#Open Files
+configIn = open("config.toml", "r")
+configOut = open("publishConfig.toml", "w")
+#Write analytics code to top
+configOut.write("[services.googleAnalytics]" + "\n"  )
+configOut.write("	id = \"UA-196724901-1\"" + "\n\n")
+#Write original file
+configOut.write(configIn.read())
+#Save files
+configIn.close()
+configOut.close()
+#Done making second config file
+
+#Run publishing scripts
+sp.run(["git", "add", "."])
+sp.run(["git", "status"])
+message = input("input git commit message:")
+sp.run(["git", "commit", "-m", message])
+sp.run(["git", "push"])
